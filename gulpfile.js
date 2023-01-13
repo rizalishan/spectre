bdc743f6d9d2cce3c7fe1aa6e306c658d932e37a
rizalishan <rizalishan@gmail.com>
2023-01-13 20:00:29 +0500

    Design rendered

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,62 @@
 const gulp = require("gulp");
 const { parallel } = require("gulp");
-const sass = require('gulp-sass');
-const cleancss = require('gulp-clean-css');
-const csscomb = require('gulp-csscomb');
-const rename = require('gulp-rename');
-const pug = require('gulp-pug');
-const autoprefixer = require('gulp-autoprefixer');
+const sass = require("gulp-sass");
+const cleancss = require("gulp-clean-css");
+const csscomb = require("gulp-csscomb");
+const rename = require("gulp-rename");
+const pug = require("gulp-pug");
+const autoprefixer = require("gulp-autoprefixer");
 
 function build() {
   return gulp
-    .src('./src/*.scss')
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
+    .src("./src/*.scss")
+    .pipe(
+      sass({ outputStyle: "compact", precision: 10 }).on("error", sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest("../template-library/src/assets/css"))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("../template-library/src/assets/css"));
 }
 
 function docs_css() {
   return gulp
-    .src(['./src/*.scss', './docs/src/scss/*.scss'])
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
+    .src(["./src/*.scss", "./docs/src/scss/*.scss"])
+    .pipe(
+      sass({ outputStyle: "compact", precision: 10 }).on("error", sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
+    .pipe(gulp.dest("./docs/dist"))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("./docs/dist"));
 }
 
 function docs_pug() {
   return gulp
-    .src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./docs/'));
+    .src("docs/src/**/!(_)*.pug")
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(gulp.dest("./docs/"));
 }
 
 function watch() {
-  gulp.watch('./**/*.scss', parallel(build, docs_css));
-  gulp.watch('./**/*.pug', docs_pug);
+  gulp.watch("./**/*.scss", parallel(build, docs_css));
+  gulp.watch("./**/*.pug", docs_pug);
 }
 
 exports.watch = watch;
